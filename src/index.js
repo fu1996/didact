d1d9e77bd93cb2d9c3e72e0bc5271374efd6ff9a
fjk <1586237690@qq.com>
2023-09-17 22:24:39 +0800

    add: 实现 任务调度（Render）和渲染节点（Commit）进行分离

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 /**
- * 一次循环到底的任务进行拆分，使用fiber单元进行细化，
- * 借助浏览器的 requestIdleCallback 的api 实现，
- * 在 js 引擎中存在长时间耗时的任务下，对任务进行中断，
- * 使其不影响浏览器响应
+ * 在V2的版本中，任务调度节点和节点渲染是在同一阶段的，可能存在一次循环渲染不完全的情况。
+ * 在当前的版本下，将任务调度和渲染节点进行分离，
+ * 实现浏览器的多次空闲时间去处理 fiber节点，
+ * 但是只有一次commit 到 真实DOM的过程，进而解决了渲染不完全的情况。
  *
- * 存在的缺点是：任务调度节点和节点渲染是在同一阶段的，可能存在渲染不完全的情况。
- *
- * 详细说明如下：
- * 因为任务节点在配合调度中心的加持下是可以中断，恢复执行的。
- * 所以可能存在如下情况：
- * 假设有100个DOM节点，要渲染到用户界面上。
- * 而在浏览器的单次循环的剩余时间下（requestIdleCallback），
- * 调度中心只能够执行了 50个节点，
- * 当浏览器剩余的空余时间不满足调度中心下次执行了，执行权交还给浏览器。
- * 此时用户仅能看到 50个DOM节点的【不完全界面】。
- * 只有在调度中心再次获取到执行权的时候，才能继续剩下 50个节点的渲染。
+ * 缺陷：目前仅仅实现了 DOM节点的新增，未实现更新和删除，V4部分要去实现更新和删除。
  */
 
 const SPECIAL_TYPE = {
@@ -64,7 +54,7 @@ function createDom(fiber) {
       ? document.createTextNode("")
       : document.createElement(fiber.type);
 
-  Object.keys(fiber.props)
+  Object.keys(fiber?.props)
     .filter(isProperty)
     .forEach((name) => {
       // 虚拟DOM的 props 绑定到真实 DOM上
@@ -72,6 +62,32 @@ function createDom(fiber) {
     });
 
   return dom;
+}
+
+// 下一个工作单元
+let nextUnitOfWork = null;
+// 循环更新的节点 work in progress root
+let wipRoot = null;
+
+/**
+ * 一次性将当前fiber节点的变更，更新为 真实的 DOM
+ */
+function commitRoot() {
+  commitWork(wipRoot.child);
+  wipRoot = null;
+  console.log("渲染结束", wipRoot);
+}
+
+/**
+ * 递归调用 将 fiber 节点更新到DOM上
+ * @param {*} fiber
+ */
+function commitWork(fiber) {
+  if (!fiber) return;
+  const domParent = fiber.parent.dom;
+  domParent.appendChild(fiber.dom);
+  commitWork(fiber.child);
+  commitWork(fiber.sibling);
 }
 
 /**
@@ -82,23 +98,20 @@ function createDom(fiber) {
 function render(element, container) {
   console.log("事件循环的起点", element, container);
   // 即将开始工作的 基础单元
-  nextUnitOfWork = {
+  wipRoot = {
     dom: container,
     props: {
       children: Array.isArray(element) ? [...element] : [element],
     },
   };
+  nextUnitOfWork = wipRoot;
 }
-
-// 下一个工作单元
-let nextUnitOfWork = null;
 
 function workLoop(deadline) {
   console.log("事件循环");
   // 是否可以继续工作
   let shouldYield = false;
   while (nextUnitOfWork && !shouldYield) {
-    //
     nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
     // 剩余时间是否小于1
     const timeRemaining = deadline.timeRemaining();
@@ -106,6 +119,13 @@ function workLoop(deadline) {
     // 如果此处的时间不足，可能导致多个节点时候的 performUnitOfWork 函数
     // 只进行了一半，就被浏览器打断了，导致界面上仅有部分节点。
     shouldYield = timeRemaining < 1;
+  }
+  // 如果下一个工作节点不存在，并且当前工作节点存在
+  // 说明本次fiber节点更新结束 可以更新为 真实的 DOM
+  if (!nextUnitOfWork && wipRoot) {
+    console.time("渲染节点");
+    commitRoot();
+    console.timeEnd("渲染节点");
   }
   // 循环的核心在此处
   requestIdleCallback(workLoop);
@@ -121,12 +141,6 @@ function performUnitOfWork(fiber) {
   // 当前fiber节点 不存在真实DOM，生成一个真实的DOM
   if (!fiber.dom) {
     fiber.dom = createDom(fiber);
-  }
-  // 如果当前节点的父级存在，就将当前虚拟DOM节点挂载在父级上
-  if (fiber.parent) {
-    // 把内容渲染到界面上。
-    console.log("渲染节点", fiber);
-    fiber.parent.dom.appendChild(fiber.dom);
   }
   // 遍历子节点，继续执行 children 属性来自于 createElement 函数。
   const elements = fiber.props.children;
